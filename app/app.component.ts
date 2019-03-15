e12d78aca3a9f6ba6a6c92a64ba2475ef030e1aa
Darshan Raul <dar.raul@gmail.com>
2019-03-15 16:03:22 +0530

    version 11

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,22 +1,36 @@
 import { Component } from "@angular/core";
 import { RouterExtensions } from "nativescript-angular/router";
+
+
 @Component({
     selector: "ns-app",
     templateUrl: "app.component.html"
 })
+
 export class AppComponent {
 
     constructor(private routerExtensions: RouterExtensions) {
     }
+    iamStyle = false;
+    s3Style = false;
+    awsStyle = true;
+    ec2Style = false;
+    rdsStyle = false;
+
 
     ngOnInit(): void {
     }
 
     onS3Tap(): void {
         this.routerExtensions.navigate(["/s3"]);
+        this.s3Style = true;
+        this.awsStyle = false;
+
     }
     onMainTap(): void {
         this.routerExtensions.navigate(["/main"]);
+        this.s3Style = false;
+        this.awsStyle = true;
     }
 
 
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule, NgModuleFactoryLoader, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
-
+import { NativeScriptHttpClientModule } from "nativescript-angular/http-client";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { DetailComponent } from "./detail/detail.component";
@@ -12,7 +12,8 @@ import { S3Component } from "./s3/s3.component";
     ],
     imports: [
         NativeScriptModule,
-        AppRoutingModule
+        AppRoutingModule,
+        NativeScriptHttpClientModule
     ],
     declarations: [
         AppComponent,
